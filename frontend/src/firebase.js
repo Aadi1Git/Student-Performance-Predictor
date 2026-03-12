9159ca19424e64cb5e0c14a7e273a7c82a0178c9
Aadi1Git <aadityajaysawal@gmail.com>
2026-03-12 16:33:06 +0530

    Secured Firebase API keys

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -1,14 +1,13 @@
-// frontend/src/firebase.js
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCZjgW9LNbh1lUFbBNF2w_-6DC4MERKVFM",
-  authDomain: "student-predictor-app.firebaseapp.com",
-  projectId: "student-predictor-app",
-  storageBucket: "student-predictor-app.firebasestorage.app",
-  messagingSenderId: "810328675754",
-  appId: "1:810328675754:web:2f06c74a6baf45062447d3"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
